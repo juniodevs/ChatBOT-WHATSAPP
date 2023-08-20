78d84298e1448a13ec79b381217e536b2c199601
Júnior Veras <iramarjunhorveras@gmail.com>
2023-08-20 18:26:13 -0300

    UPDATE USER

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import requests
 
 agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 
-usuario = 'iramarjunhorveras@gmail.com'
+usuario = 'USER'
 dir_path = os.getcwd()
 chrome_options2 = Options()
 chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/pasta/sessao")
